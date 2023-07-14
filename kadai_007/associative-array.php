0aaa15ef77159c9841bfcd8aa39ed98801f234eb
Kakeru Mino <96040265+kakeru72527@users.noreply.github.com>
2023-07-14 21:36:32 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset='utf-8'>
+    <title>php課題007</title>
+  </head>
+
+  <body>
+    <?php
+      // 連想配列を作成
+      $items = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      // 出力
+      print_r($items);
+    ?>
+
+  </body>
+</html>
